1b860f7fa8055eea9c11fe060a62c357b683a4f7
LinKzy <danilo.ut@gmail.com>
2023-04-13 13:48:37 -0300

    updating models

--- BertTest/Program.cs
+++ BertTest/Program.cs
@@ -2,15 +2,24 @@
 using System;
 using System.Text.Json;
 
+Console.WriteLine("Initializing Model...");
 var model = new Bert(
     Directory.GetCurrentDirectory() + "\\Assets\\Vocabulary\\vocab.txt",
     Directory.GetCurrentDirectory() + "\\Assets\\Model\\bertsquad-10.onnx"
     );
 
-var (tokens, probability) = model.Predict("Jim is walking through the woods.", "What is his name?");
+string text = "Jim is walking through the woods";
+
+Console.WriteLine("Predicting...");
+var (tokens, probability) = model.Predict(
+    text, 
+    "Who was him?");
 
 Console.WriteLine(JsonSerializer.Serialize(new
 {
     Probability = probability,
     Tokens = tokens
 }));
+
+Console.WriteLine("Done.");
+Console.ReadLine();
